63fa4fbfc4b415c035df7b1297641ef861f4e477
JFMajer <jakubmajer@live.com>
2022-02-13 21:12:45 +0100

    reading keyboard inputs

---
+++ test-integer-with-input.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -n "Please enter an integer -> "
+read int
+
+if [[ $int =~ ^-?[0-9]+$ ]]; then
+    if [[ $int == 0 ]]; then
+        echo "int $int is equal to zero."
+    elif [[ $int < 0 ]]; then
+        echo "int $int is negative."
+    else
+        echo "int $int is positive."
+    fi
+else
+    echo "$int is not really an integer.">&2
+    exit 1
+fi
+
