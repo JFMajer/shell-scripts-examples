00746ccfab14a1a104b582d947377f02c2f88489
JFMajer <jakubmajer@live.com>
2022-09-13 19:47:01 +0200

    positional arguments

---
+++ positional_arguments.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# $# - num of positional arguments
+# "$*" - all positional arguments (as a string)
+
+if [[ "$#" -eq 0  ]]; then
+  echo "Please provide at least one argument to test this script"
+  exit 0
+fi
+
+
+echo "\$* is $*"
+echo "\$# is $#"
+echo "\$@ is $@"
+
+echo "Listing arguments..."
+
+for i in "$@" 
+do
+  echo "$i"
+done
