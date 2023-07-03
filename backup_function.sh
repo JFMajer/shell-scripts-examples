51c92080ce32d9e83526b9916c7f6eedc71d9c72
JFMajer <jakubmajer@live.com>
2023-07-03 20:36:19 +0200

    backup current dir (function)

---
+++ backup_function.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+perform_backup() {
+    backup_dir="backup_$(date +'%Y%m%d_%H%M%S')"
+    mkdir "$backup_dir"
+    tar_file="$backup_dir/backup_$(date +'%Y%m%d_%H%M%S').tar.gz"
+    tar -czvf "$tar_file" .
+    
+    echo "Backup created: ${tar_file}"
+}
+
+perform_backup
